70d24b254bf25135b9896584127d5bc12d3f5bf1
nnorwitz <nnorwitz>
2001-04-09 16:28:42 +0000

    move tests to their own sub-dir

---
+++ test_input/test3.py
@@ -0,0 +1,12 @@
+
+import string
+
+def describeSyntax(syntax):
+    return string.join(['<%s>' % x.Description for x in syntax])
+
+from sre import Scanner
+
+class jj(Scanner) :
+    def __init__(self):
+      Scanner.__init__(self, None)
+
