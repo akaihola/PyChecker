1045bbe4c6b1292d13000b41d17aa5976e8475b0
nnorwitz <nnorwitz>
2001-04-13 15:48:05 +0000

    fix doc string, put at right indentation

--- test_input/test5.py
+++ test_input/test5.py
@@ -9,6 +9,6 @@ class X:
     def __init__(self):
         self.y = 0
     def z(self):
-    "this should not have any warnings"
+        "this should not have any warnings"
         x(self.y, { 'a': 'b' })
 
