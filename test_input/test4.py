c6c8090e1450d8e6c1ba478fbd89aa441e6efc6f
nnorwitz <nnorwitz>
2001-04-16 19:29:14 +0000

    update test

--- test_input/test4.py
+++ test_input/test4.py
@@ -2,11 +2,21 @@
 import UserDict 
 
 class jj(UserDict.UserDict) :
+    "jj"
     def __init__(self):
       UserDict.UserDict.__init__(self, None)
 
 class jj2(UserDict.UserDict) :
+    "jj2"
     def __init__(self):
       "Warning don't call UserDict.UserDict.__init__()"
       print "don't call UserDict.UserDict.__init__()"
+
+class jj3(jj) :
+    "jj3"
+    def __init__(self):
+      jj.__init__(self)
+
+class jj4(jj) :
+    "jj4"
 
