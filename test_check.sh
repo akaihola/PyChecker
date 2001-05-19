3a0b024519d518d03ce3665b8b69707cb698583e
nnorwitz <nnorwitz>
2001-05-19 23:31:04 +0000

    Don't say test failed if there's no expected results, print warning

--- test_check.sh
+++ test_check.sh
@@ -12,9 +12,15 @@ FAILED=""
 for test_file in $TESTS ; do
     echo "Testing $test_file ..."
     test_name=`basename $test_file .py`
+    expected=test_expected/$test_name
+    if [ ! -e $expected ]; then
+        echo "  WARNING:  $expected expected results does not exist"
+	continue
+    fi
+
     test_path=$TMP/$test_name
     ./pychecker/checker.py $test_file > $test_path 2>&1
-    diff $test_path test_expected/$test_name
+    diff $test_path $expected
     if [ $? -ne 0 ]; then
     	error=`expr $error + 1`
 	echo "  $test_name FAILED"
