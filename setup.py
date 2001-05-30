02856df8a9a013b033cae754718f79aabe3bbc1c
nnorwitz <nnorwitz>
2001-05-30 01:49:59 +0000

    bump version for release

--- setup.py
+++ setup.py
@@ -11,15 +11,23 @@ Contributed by:  Nicolas Chauvat
 from distutils.core import setup
 
 if __name__ == '__main__' :
-  setup(name		= "PyChecker",
-        version		= "0.4beta",
-        license		= "BSD-like",
-        description	= "Python source code checking tool",
-        author		= "Neal Norwitz, MetaSlash, Inc.",
-        author_email	= "pychecker@metaslash.com",
-        url		= "http://pychecker.sourceforge.net/",
-        packages	= [ 'pychecker' ],
-	#doc_files	= [ 'COPYRIGHT', 'VERSION', 'README', ],
-        long_description = "PyChecker is a python source code checking tool to help you find common bugs. It is meant to find problems that are typically caught by a compiler. Because of the dynamic nature of python, some warnings may be incorrect; however, spurious warnings should be fairly infrequent."
-        )
+    DOC_FILES = [ 'COPYRIGHT', 'README', 'VERSION', ]
+    LONG_DESCRIPTION = \
+"""PyChecker is a tool for finding common bugs in python source code.
+It finds problems that are typically caught by a compiler for less
+dynamic languages, like C and C++. Because of the dynamic nature of python,
+some warnings may be incorrect; however, spurious warnings should be
+fairly infrequent."""
+
+    setup(name			= "PyChecker",
+	  version		= "0.5",
+	  license		= "BSD-like",
+	  description		= "Python source code checking tool",
+	  author		= "Neal Norwitz, MetaSlash, Inc.",
+	  author_email		= "pychecker@metaslash.com",
+	  url			= "http://pychecker.sourceforge.net/",
+	  packages		= [ 'pychecker' ],
+	  #doc_files		= DOC_FILES,
+	  long_description	= LONG_DESCRIPTION
+	 )
 
