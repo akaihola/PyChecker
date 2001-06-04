44382f6055b57c5c2a62bfbc5eac3badeab945a1
nnorwitz <nnorwitz>
2001-06-04 02:32:56 +0000

    fix partial sentance

--- pychecker/__init__.py
+++ pychecker/__init__.py
@@ -3,7 +3,7 @@ Copyright (c) 2001, MetaSlash Inc.  All rights reserved.
 
 PyChecker is a tool for finding common bugs in python source code.
 It finds problems that are typically caught by a compiler for less
-dynamic languages, like C and C++.  Because of the dynamic nature
+dynamic languages, like C and C++.  It is also similar to lint.
 
 Contact Info:
   http://pychecker.sourceforge.net/
