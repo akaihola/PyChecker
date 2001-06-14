e0301fc401857bc1453be6822f9384f710884e7b
nnorwitz <nnorwitz>
2001-06-14 00:04:10 +0000

    install script to /usr/local/bin to invoke PyChecker

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python
 
 """To use this setup script to install PyChecker:
 
@@ -8,7 +8,11 @@
 Contributed by:  Nicolas Chauvat
 """
 
+import sys
+import os
+
 from distutils.core import setup
+from distutils import sysconfig
 
 if __name__ == '__main__' :
     DOC_FILES = [ 'COPYRIGHT', 'README', 'VERSION', ]
@@ -19,6 +23,30 @@ dynamic languages, like C and C++. Because of the dynamic nature of python,
 some warnings may be incorrect; however, spurious warnings should be
 fairly infrequent."""
 
+    script_suffix = 'sh'
+    if sys.platform == 'win32' :
+        script_suffix = 'bat'
+    LOCAL_SCRIPT = 'pychecker.' + script_suffix
+    try :
+	os.unlink(LOCAL_SCRIPT)
+    except :
+        pass
+
+    install_dir = sysconfig.get_python_lib() + os.sep + 'pychecker'
+    checker_py = install_dir + os.sep + 'checker.py'
+    py_exe = sys.executable
+    script_str = '#! /bin/sh\n\n%(py_exe)s %(checker_py)s $*\n' % locals()
+
+    try :
+        fp = open(LOCAL_SCRIPT, "w")
+	fp.write(script_str)
+	fp.close()
+	if sys.platform != 'mac' :
+	    os.chmod(LOCAL_SCRIPT, 0755)
+    except :
+        print "Unable to create utility script."
+	raise
+
     setup(name			= "PyChecker",
 	  version		= "0.5.1beta",
 	  license		= "BSD-like",
@@ -27,7 +55,8 @@ fairly infrequent."""
 	  author_email		= "pychecker@metaslash.com",
 	  url			= "http://pychecker.sourceforge.net/",
 	  packages		= [ 'pychecker' ],
-	  #doc_files		= DOC_FILES,
+	  #data_files		= DOC_FILES,
+	  scripts		= [ LOCAL_SCRIPT, ],
 	  long_description	= LONG_DESCRIPTION
 	 )
 
