05deaefa5dec877fb2181f237e385153c92fdc63
nnorwitz <nnorwitz>
2001-07-05 22:20:51 +0000

    use -e not -S for --self param, so -S can be strict (tbd)

--- pychecker/Config.py
+++ pychecker/Config.py
@@ -34,7 +34,7 @@ _OPTIONS = [
  ('I', 0, 'initsubclass', 'initDefinedInSubclass', 'Subclass.__init__() not defined'),
  ('N', 0, 'initreturn', 'returnNoneFromInit', 'Return None from __init__()'),
  ('A', 0, 'callattr', 'callingAttribute', 'Calling data members as functions'),
- ('S', 1, 'self', 'methodArgName', 'First argument to methods'),
+ ('e', 1, 'self', 'methodArgName', 'First argument to methods'),
  None,
  ('b', 1, 'blacklist', 'blacklist', 'ignore warnings from the list of modules\n\t\t\t'),
  ('V', 1, 'varlist', 'variablesToIgnore', 'ignore variables not used from the list\n\t\t\t'),
