e6ad25bb04eacadf0fc32085c22289debb832aeb
nnorwitz <nnorwitz>
2001-09-24 19:43:07 +0000

    start to remove dependancy on dis module

--- pychecker/OP.py
+++ pychecker/OP.py
@@ -3,83 +3,70 @@
 # Copyright (c) 2001, MetaSlash Inc.  All rights reserved.
 
 """
-Python code operations.  This is a convenience wrapper around dis.
+Python byte code operations.
+
+Very similar to the dis module, but dis does not exist in Jython,
+so recreate the small portion we need here.
 """
 
 import dis
 
-def LINE_NUM(op):              return dis.opname[op] == 'SET_LINENO'
-def LOAD_GLOBAL(op):           return dis.opname[op] == 'LOAD_GLOBAL'
-def LOAD_CONST(op):            return dis.opname[op] == 'LOAD_CONST'
-def LOAD_DEREF(op):            return dis.opname[op] == 'LOAD_DEREF'
-def LOAD_NAME(op):             return dis.opname[op] == 'LOAD_NAME'
-def LOAD_FAST(op):             return dis.opname[op] == 'LOAD_FAST'
-def LOAD_ATTR(op):             return dis.opname[op] == 'LOAD_ATTR'
-def STORE_ATTR(op):            return dis.opname[op] == 'STORE_ATTR'
-def STORE_FAST(op):            return dis.opname[op] == 'STORE_FAST'
-def STORE_NAME(op):            return dis.opname[op] == 'STORE_NAME'
-def STORE_GLOBAL(op):          return dis.opname[op] == 'STORE_GLOBAL'
-def STORE_SUBSCR(op):          return dis.opname[op] == 'STORE_SUBSCR'
-def CALL_FUNCTION(op):         return dis.opname[op] == 'CALL_FUNCTION'
-def IMPORT_NAME(op):           return dis.opname[op] == 'IMPORT_NAME'
-def IMPORT_FROM(op):           return dis.opname[op] == 'IMPORT_FROM'
-def IMPORT_STAR(op):           return dis.opname[op] == 'IMPORT_STAR'
-def UNARY_POSITIVE(op):        return dis.opname[op] == 'UNARY_POSITIVE'
-def UNARY_NEGATIVE(op):        return dis.opname[op] == 'UNARY_NEGATIVE'
-def UNARY_INVERT(op):          return dis.opname[op] == 'UNARY_INVERT'
-def BINARY_SUBSCR(op):         return dis.opname[op] == 'BINARY_SUBSCR'
-def BINARY_ADD(op):            return dis.opname[op] == 'BINARY_ADD'
-def BUILD_LIST(op):            return dis.opname[op] == 'BUILD_LIST'
-def BUILD_MAP(op):             return dis.opname[op] == 'BUILD_MAP'
-def BUILD_TUPLE(op):           return dis.opname[op] == 'BUILD_TUPLE'
-def RETURN_VALUE(op):          return dis.opname[op] == 'RETURN_VALUE'
-def COMPARE_OP(op):            return dis.opname[op] == 'COMPARE_OP'
-def POP_TOP(op):               return dis.opname[op] == 'POP_TOP'
-def DUP_TOP(op):               return dis.opname[op] == 'DUP_TOP'
-def FOR_LOOP(op):              return dis.opname[op] == 'FOR_LOOP'
-def JUMP_FORWARD(op):          return dis.opname[op] == 'JUMP_FORWARD'
-def RAISE_VARARGS(op):         return dis.opname[op] == 'RAISE_VARARGS'
+name = dis.opname
+
+def LINE_NUM(op):              return name[op] == 'SET_LINENO'
+def LOAD_GLOBAL(op):           return name[op] == 'LOAD_GLOBAL'
+def LOAD_CONST(op):            return name[op] == 'LOAD_CONST'
+def LOAD_FAST(op):             return name[op] == 'LOAD_FAST'
+def LOAD_ATTR(op):             return name[op] == 'LOAD_ATTR'
+def STORE_ATTR(op):            return name[op] == 'STORE_ATTR'
+def IMPORT_FROM(op):           return name[op] == 'IMPORT_FROM'
+def IMPORT_STAR(op):           return name[op] == 'IMPORT_STAR'
+def UNARY_POSITIVE(op):        return name[op] == 'UNARY_POSITIVE'
+def UNARY_NEGATIVE(op):        return name[op] == 'UNARY_NEGATIVE'
+def UNARY_INVERT(op):          return name[op] == 'UNARY_INVERT'
+def RETURN_VALUE(op):          return name[op] == 'RETURN_VALUE'
+def JUMP_FORWARD(op):          return name[op] == 'JUMP_FORWARD'
+def RAISE_VARARGS(op):         return name[op] == 'RAISE_VARARGS'
 
 def UNPACK_SEQUENCE(op) :
     "Deal w/Python 1.5.2 (UNPACK_[LIST|TUPLE]) or 2.0 (UNPACK_SEQUENCE)"
-    return dis.opname[op] in ['UNPACK_SEQUENCE', 'UNPACK_TUPLE', 'UNPACK_LIST']
+    return op in [ 92, 93, ]
 
-HAVE_ARGUMENT = dis.HAVE_ARGUMENT
+HAVE_ARGUMENT = 90
+EXTENDED_ARG = 143
 
-try:
-    # EXTENDED_ARG is a Python2.0 feature
-    EXTENDED_ARG = dis.EXTENDED_ARG
-except:
-    EXTENDED_ARG = None
+_HAS_NAME = [ 90, 91, 95, 96, 97, 98, 101, 105, 107, 108, 116, ]
+_HAS_LOCAL = [ 124, 125, 126, ]
+_HAS_CONST = [ 100, ]
+_HAS_COMPARE = [ 106, ]
+_HAS_JREL = [ 110, 111, 112, 114, 120, 121, 122, ]
+_HAS_JABS = [ 113, 119, ]
 
-hasname = dis.hasname
-haslocal = dis.haslocal
-hasconst = dis.hasconst
-hascompare = dis.hascompare
-name = dis.opname
+_CMP_OP =  ('<', '<=', '==', '!=', '>', '>=', 'in', 'not in', 'is',
+            'is not', 'exception match', 'BAD')
 
 def getOperand(op, func_code, oparg) :
-    if op in hasname :
+    if op in _HAS_NAME :
         return func_code.co_names[oparg]
-    elif op in haslocal :
+    elif op in _HAS_LOCAL :
         return func_code.co_varnames[oparg]
-    elif op in hasconst :
+    elif op in _HAS_CONST :
         return func_code.co_consts[oparg]
-    elif op in hascompare :
-        return dis.cmp_op[oparg]
+    elif op in _HAS_COMPARE :
+        return _CMP_OP[oparg]
     return None
 
 def getLabel(op, oparg, i) :
-    if op in dis.hasjrel :
+    if op in _HAS_JREL :
         return i + oparg
-    elif op in dis.hasjabs :
+    elif op in _HAS_JABS :
         return oparg
     return None
 
 def getInfo(code, index, extended_arg) :
     """Returns (op, oparg, index, extended_arg) based on code
-         this is a helper function while looping through byte code,
-         refer to the standard module dis.disassemble() for more info"""
+       this is a helper function while looping through byte code,
+       refer to the standard module dis.disassemble() for more info"""
 
     # get the operation we are performing
     op = ord(code[index])
@@ -90,17 +77,16 @@ def getInfo(code, index, extended_arg) :
         index = index + 2
         extended_arg = 0
         if op == EXTENDED_ARG :
-        	extended_arg = oparg * 65536L
+            extended_arg = oparg * 65536L
     else :
         oparg, extended_arg = 0, 0
     return op, oparg, index, extended_arg
 
 def initFuncCode(func) :
     """Returns (func_code, code, i, maxCode, extended_arg) based on func,
-         this is a helper function to setup looping through byte code"""
+       this is a helper function to setup looping through byte code"""
 
     func_code = func.func_code
     code = func_code.co_code
     return func_code, code, 0, len(code), 0
 
-    
