cb91b472513984dcaa80a518c630b2ad6f54ece1
nnorwitz <nnorwitz>
2002-01-09 16:17:50 +0000

    fail a little more gracefully for non-2.2 interpreters

--- test_input/test75.py
+++ test_input/test75.py
@@ -4,16 +4,20 @@ class A:
     'warn about using slots in classic classes'
     __slots__ = ('a',)
 
-class B(object):
-    'no warning'
-    __slots__ = ('a',)
+try:
+    class B(object):
+        'no warning'
+        __slots__ = ('a',)
 
-class C(object):
-    'warn about using empty slots'
-    __slots__ = ()
+    class C(object):
+        'warn about using empty slots'
+        __slots__ = ()
 
-class D(object):
-    "don't warn about using empty slots"
-    __pychecker__ = '--no-emptyslots'
-    __slots__ = ()
+    class D(object):
+        "don't warn about using empty slots"
+        __pychecker__ = '--no-emptyslots'
+        __slots__ = ()
+
+except NameError:
+    print 'This should fail on Python versions prior to 2.2'
 
