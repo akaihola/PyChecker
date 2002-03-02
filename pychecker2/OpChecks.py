030ad30a702ae18d1755f0fe8c35fc32042b7f85
ericnewton <ericnewton>
2002-03-02 21:43:13 +0000

    massively simplify operator warnings

--- pychecker2/OpChecks.py
+++ pychecker2/OpChecks.py
@@ -1,54 +1,29 @@
 from pychecker2 import Check
-from pychecker2.util import BaseVisitor
+from pychecker2.Warning import Warning
 
 import compiler
 
-class FindVisitor(BaseVisitor):
-
-    def __init__(self):
-        self.nodes = []
-
-    def append(self, n):
-        self.nodes.append(n)
-    
-
-class PlusPlus(FindVisitor):
-
-    def visitUnaryAdd(self, n):
-        if n.getChildren()[0].__class__ == compiler.ast.UnaryAdd:
-            self.append(n)
-        self.visitChildren(n)
-
-class MinusMinus(FindVisitor):
-
-    def visitUnarySub(self, n):
-        if n.getChildren()[0].__class__ == compiler.ast.UnarySub:
-            self.append(n)
-        self.visitChildren(n)
-
-class Plus(FindVisitor):
-
-    def visitUnaryAdd(self, n):
-        if n.getChildren()[0].__class__ != compiler.ast.UnaryAdd:
-            self.append(n)
-        self.visitChildren(n)
 
 class OpCheck(Check.Check):
 
-    def __init__(self, visitor_class, msg):
-        self.visitor_class = visitor_class
-        self.msg = msg
-
-    def __str__(self):
-        return self.visitor_class.__name__.split('.')[-1]
+    opWarning = Warning('operator',
+                        "Operator (%s) doesn't exist, statement has no effect")
+    plusWarning = Warning('operatorPlus',
+                          "Operator (+) normally has no effect")
 
     def check(self, file, unused_options):
-        if not file.parseTree:
-            return
-        for p in compiler.walk(file.parseTree, self.visitor_class()).nodes:
-            file.warning(p, self.msg)
+        class OpVisitor:
+            def visitUnaryAdd(self, n):
+                if n.getChildren()[0].__class__ == compiler.ast.UnaryAdd:
+                    file.warning(n, OpCheck.opWarning, '++')
+                else:
+                    file.warning(n, OpCheck.plusWarning)
 
-Check.pass1.append(OpCheck(PlusPlus,  "Operator (++) doesn't exist, statement has no effect"))
-Check.pass1.append(OpCheck(Plus,      "Operator (+) normally has no effect"))
-Check.pass1.append(OpCheck(MinusMinus,"Operator (--) doesn't exist, statement has no effect"))
+            def visitUnarySub(self, n):
+                if n.getChildren()[0].__class__ == compiler.ast.UnarySub:
+                    file.warning(n, OpCheck.opWarning, '--')
+        if file.parseTree:        
+            compiler.walk(file.parseTree, OpVisitor())
+
+Check.pass1.append(OpCheck())
 
