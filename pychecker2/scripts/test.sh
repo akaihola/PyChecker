879f257653eee04f16d9252effba364a0852976f
ericnewton <ericnewton>
2002-03-02 22:55:09 +0000

    check all warning options

--- pychecker2/scripts/test.sh
+++ pychecker2/scripts/test.sh
@@ -16,8 +16,11 @@ fi
 eval $CHECKER tests/*.py $OUTPUT tests/expected/normal  
 eval $CHECKER -?         $OUTPUT tests/expected/options 
 
-for opt in verbose shadowBuiltins reportUnusedSelf
+for opt in verbose shadowBuiltins reportUnusedSelf \
+    no-operator no-operatorPlus no-redefinedScope no-shadowIdentifier \
+    no-syntaxErrors no-unknown no-unused 
 do
+   echo $opt
    eval $CHECKER --${opt} tests/*.py tests/not_there.py $OUTPUT tests/expected/$opt
 done
 
