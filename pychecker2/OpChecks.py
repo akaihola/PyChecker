88e7e861b6eefa1740d8a30d092a6e17cd5ac854
ericnewton <ericnewton>
2002-03-03 15:35:23 +0000

    unused -> self, because the first arg of a method should be self

--- pychecker2/OpChecks.py
+++ pychecker2/OpChecks.py
@@ -17,13 +17,13 @@ class OpCheck(Check.Check):
 
     def check(self, unused_modules, file, unused_options):
         class OpVisitor:
-            def visitUnaryAdd(unused, n):
+            def visitUnaryAdd(self, n):
                 if n.getChildren()[0].__class__ == compiler.ast.UnaryAdd:
                     file.warning(n, OpCheck.operator, '++')
                 else:
                     file.warning(n, OpCheck.operatorPlus)
 
-            def visitUnarySub(unused, n):
+            def visitUnarySub(self, n):
                 if n.getChildren()[0].__class__ == compiler.ast.UnarySub:
                     file.warning(n, OpCheck.operator, '--')
         if file.parseTree:        
