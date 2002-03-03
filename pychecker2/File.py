a1eb5d33393c4f2ac5c9ab0c35c874f912c52ef7
ericnewton <ericnewton>
2002-03-03 00:02:25 +0000

    Get rid of shadow warning

--- pychecker2/File.py
+++ pychecker2/File.py
@@ -11,11 +11,11 @@ class File:
     def __cmp__(self, other):
         return cmp(self.name, other.name)
 
-    def warning(self, line, warning, *args):
+    def warning(self, line, warn, *args):
 	try:
 	    line = line.lineno
         except AttributeError:
 	    pass
-        self.warnings.append( (line, warning, args) )
+        self.warnings.append( (line, warn, args) )
 
 
