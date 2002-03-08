e7d86ac49ff177daa74d85bf6609604886d7c5aa
ericnewton <ericnewton>
2002-03-08 23:44:58 +0000

    a better error message

--- pychecker2/util.py
+++ pychecker2/util.py
@@ -3,7 +3,7 @@ class BaseVisitor:
 
     def visit(self, node):
         "method is really overridden by compiler.visitor.ASTVisitor"
-        raise NotImplementedError("Unreachable")
+        raise NotImplementedError("This should have been overridden")
 
     def visitChildren(self, n):
         for c in n.getChildNodes():
