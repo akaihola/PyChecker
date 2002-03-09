0629d2b3ba91db54893c431e16cd8f049988c378
ericnewton <ericnewton>
2002-03-09 17:08:29 +0000

    functions that only assert 0 should not generate unused arg warnings

--- pychecker2/tests/unused.py
+++ pychecker2/tests/unused.py
@@ -11,3 +11,11 @@ def g():
             self.foo = TestInner()
             
                     
+x, y = 1, 2
+class Object: pass
+x = Object()
+x.y, x.z = 1, 2
+
+def h(a, b, c):
+    assert 0, "should not be called"
+
