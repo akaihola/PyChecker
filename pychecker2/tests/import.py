2f1e0364b9017d94abf5fbd1234837fcf9e74287
ericnewton <ericnewton>
2002-03-10 15:13:47 +0000

    more import tests to handle all kinds of import styles stop warning about global names, except if they are imports

--- pychecker2/tests/import.py
+++ pychecker2/tests/import.py
@@ -1,6 +1,7 @@
 from os.path import *
-from sys import path as foo
-import getopt                           # unused
+from sys import path as foo, argv as bar# bar not used
+import getopt                           # getopt not used
+import os.path as nifty                 # nifty not used
 
 def f(v):
     v.append(join(foo))                 # join from os.path
@@ -8,5 +9,8 @@ def f(v):
 
 x = 7
 if x == 100:
-    from xYzZy import *
+    from xYzZy import *                 # not found
     x = z
+
+not_used = 13
+
