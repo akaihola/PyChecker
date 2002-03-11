95a188de13a7e278614937efb54331a8568a2393
ericnewton <ericnewton>
2002-03-11 02:35:31 +0000

    add duplicate import warnings

--- pychecker2/ImportChecks.py
+++ pychecker2/ImportChecks.py
@@ -13,6 +13,10 @@ Figure out which names come from 'import name'.
 
     importError = Warning('Report/ignore imports that may fail',
                           'Error trying to import %s: %s')
+    duplicateImport = Warning('Report/ignore duplicate imports',
+                              'Import of "%s" is duplicate%s')
+    shadowImport = Warning('Report imports which shadow names from other imports',
+                              'Import of "%s" duplicates import from module %s at %d')
 
     def check(self, unused_modules, file, unused_options):
         def try_import(name, node):
@@ -21,6 +25,23 @@ Figure out which names come from 'import name'.
             except ImportError, detail:
                 file.warning(node, ImportCheck.importError, name, detail)
                 return None
+        def add_import(scopes, node, name, module):
+            for scope in scopes:
+                try:
+                    smodule, snode = scope.imports[name]
+                    if smodule == module:
+                        if scope == scopes[0]:
+                            extra = " in current scope"
+                        else:
+                            extra = " of import in parent scope %s" % scope
+                        file.warning(node, ImportCheck.duplicateImport,
+                                     name, extra)
+                    else:
+                        file.warning(node, ImportCheck.shadowImport,
+                                     name, smodule.__name__, snode.lineno)
+                except KeyError:
+                    pass
+            scopes[0].imports[name] = (module, node)
             
         class FromImportVisitor:
 
@@ -31,9 +52,10 @@ Figure out which names come from 'import name'.
                         if module_name == '*':
                             for name in dir(m):
                                 if not name.startswith('_'):
-                                    scopes[-1].imports[name] = m
+                                   add_import(scopes, node, name, m)
                         else:
-                            scopes[-1].imports[local_name or module_name] = m
+                            add_import(scopes, node,
+                                       local_name or module_name, m)
 
             def visitImport(self, node, *scopes):
                 for module, name in node.names:
@@ -41,7 +63,7 @@ Figure out which names come from 'import name'.
                     if m:
                         # example: os.path stored under "os" or supplied name
                         base = module.split('.')[0]
-                        scopes[-1].imports[name or base] = m
+                        add_import(scopes, node, name or base, m)
 
 
         if file.root_scope:
--- pychecker2/tests/import.py
+++ pychecker2/tests/import.py
@@ -1,9 +1,10 @@
-from os.path import *
 from sys import path as foo, argv as bar# bar not used
 import getopt                           # getopt not used
 import os.path as nifty                 # nifty not used
+from os.path import *
 
 def f(v):
+    from os.path import join            # duplicate import
     v.append(join(foo))                 # join from os.path
     v.append(__all__)                   # __all__ is unknown
 
@@ -13,4 +14,6 @@ if x == 100:
     x = z
 
 not_used = 13
+import getopt                           # duplicate import
+from string import *                    # shadow 'join'
 
