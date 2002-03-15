26fd4215411b5e1ba5bc36eae019bb105c53967e
ericnewton <ericnewton>
2002-03-15 00:47:10 +0000

    ScopeVisitor is history, use parent reference Add function to find out if two statement are in a simple if/try

--- pychecker2/util.py
+++ pychecker2/util.py
@@ -1,4 +1,5 @@
 import types
+import compiler.ast
 
 class BaseVisitor:
 
@@ -10,29 +11,6 @@ class BaseVisitor:
         for c in n.getChildNodes():
             self.visit(c)     
 
-class ScopeVisitor:
-
-    def __init__(self, scopes, visitor):
-        self.scopes = scopes
-        self.visitor = visitor
-
-    def visit(self, unused_node):
-        "method is really overridden by compiler.visitor.ASTVisitor"
-        pass
-
-    # return default for any name that isn't in visitor
-    def __getattr__(self, name):
-        return getattr(self.visitor, name, self.default)
-
-    # Add a scope, if the node corresponds to a new scope
-    def default(self, node, *scopes):
-        try:
-            scopes = (self.scopes[node],) + scopes
-        except KeyError:
-            pass
-        for c in node.getChildNodes():
-            self.visit(c, *scopes)
-
 # http://mail.python.org/pipermail/python-list/2000-December/023319.html
 # Fredric Lundh
 def flatten(seq):
@@ -43,3 +21,24 @@ def flatten(seq):
         else:
             res.append(item)
     return res
+
+def enclosing_scopes(scopes, node):
+    result = []
+    n = node
+    while n:
+        try:
+            result.append(scopes[n])
+        except KeyError:
+            pass
+        n = n.parent
+    return result
+
+def under_simple_try_if(stmt_node1, stmt_node2):
+    try:
+        if stmt_node1.parent.parent == stmt_node2.parent.parent and \
+           isinstance(stmt_node1.parent.parent,
+                      (compiler.ast.TryExcept, compiler.ast.If)):
+            return 1
+    except AttributeError:
+        pass
+    return None
