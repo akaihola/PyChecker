d760b1dc5bbcbe590b4982f125513b10300f4403
nnorwitz <nnorwitz>
2002-03-20 18:50:57 +0000

    bump for release

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ fairly infrequent."""
         raise
 
     setup(name                  = "PyChecker",
-          version               = "0.9.0beta",
+          version               = "0.8.10",
           license               = "BSD-like",
           description           = "Python source code checking tool",
           author                = "Neal Norwitz, MetaSlash, Inc.",
