547cd0076ca34f7493b60d5cd464209c9a380b6e
ericnewton <ericnewton>
2002-03-26 19:33:58 +0000

    tests for format strings

---
+++ pychecker2/tests/format.py
@@ -0,0 +1,40 @@
+
+def f1(x):
+    return '%s' % x
+
+def f2(x):
+    return ('%s' + '%s') % (x, x)
+
+def f3(x):
+    return (('%s' + '%s') * 8) % ((x,) * 16)
+
+def f4():
+    return '%(x)f %(y)s' % locals()
+
+def f5():
+    return '%' % locals()
+
+def f6():
+    return '%z this, too is a kookie format, yah' % locals()
+
+def f7():
+    return '%*.s %*.*s %*f' % locals()
+
+def f8():
+    return '%(mi)x %(it)s %up' % locals()
+
+def f9():
+    return '%s %d %f' % ('', 2)
+
+def f10():
+    return '%s %%' % ('',)
+
+def f11(t):
+    return '%s %f' % t
+
+def f12(t):
+    return '%s %f' % (t, t)
+
+def f13(t):
+    return ('%s %f' + t) % (1, 2)
+
