2fc09198574eb2d2415fbc4fc4ab38ae371fa80d
ericnewton <ericnewton>
2002-04-02 19:46:31 +0000

    add test for redefinition of a method

--- pychecker2/utest/scopes.py
+++ pychecker2/utest/scopes.py
@@ -1,10 +1,14 @@
 from pychecker2.TestSupport import WarningTester
 from pychecker2 import ScopeChecks
 
-class UnusedTestCase(WarningTester):
+class RedefinedTestCase(WarningTester):
     def testScopes(self):
         self.warning('def f(): pass\n'
                      'def f(): pass\n',
                      1, ScopeChecks.RedefineCheck.redefinedScope, 'f', 2)
+        self.warning('class C:\n'
+                     '  def g(self): pass\n'
+                     '  def g(self): pass\n',
+                     2, ScopeChecks.RedefineCheck.redefinedScope, 'g', 3)
         self.silent('def s(): pass\n'
                     'def f(): pass\n')
