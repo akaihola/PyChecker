24da07be10faf3d8132f6b31da92e985819ba9ab
ericnewton <ericnewton>
2002-04-04 17:59:37 +0000

    exercise unreachable conditions

---
+++ pychecker2/utest/reachable.py
@@ -0,0 +1,51 @@
+from pychecker2.TestSupport import WarningTester
+from pychecker2 import ReachableChecks
+
+class ReachableTestCase(WarningTester):
+    def testImportChecks(self):
+        w = ReachableChecks.ReachableCheck.unreachable
+        self.warning('v1, v2 = 0, 0\n'
+                     'def f():\n'
+                     '  if v1:\n'
+                     '     return v2\n'
+                     '  elif v2:\n'
+                     '    return 1\n'
+                     '  else:\n'
+                     '    return 0\n'
+                     '  return 1\n', 9, w)
+        self.warning('v1, v2 = 0, 0\n'
+                     'def f():\n'
+                     '  if v1:\n'
+                     '    raise AssertionError("assertion")\n'
+                     '  elif v2:\n'
+                     '    assert 0, "Another error"\n'
+                     '  else:\n'
+                     '    return 0\n'
+                     '  return f()\n', 9, w)
+        self.silent('def f(x):\n'
+                    '   while x:\n'
+                    '      pass\n')
+        self.silent('def f(x):\n'
+                    '   for i in x:\n'
+                    '      assert i\n')
+        self.silent('def f(x):\n'
+                    '  if x:\n'
+                    '    return 0\n'
+                    '  return 0\n')
+        self.warning('def f(x):\n'
+                     '  try:\n'
+                     '    x = 1 / x\n'
+                     '    return x\n'
+                     '  except Exception:\n'
+                     '    return 0\n'
+                     '  else:\n'
+                     '    return 42\n', 8, w)
+
+        self.silent('def f(v1, v2):\n'
+                    '  if v1:\n'
+                    '    return v2\n'
+                    '  elif v2:\n'
+                    '    v1 = v2\n'
+                    '  else:\n'
+                    '    assert 0, "Another error"\n'
+                    '  return f()\n')
