368b13c1e28350f46ab65e045cca65a39cafb25a
ericnewton <ericnewton>
2002-04-04 18:44:16 +0000

    cover the removal of "main" from modules

--- pychecker2/utest/parseerror.py
+++ pychecker2/utest/parseerror.py
@@ -1,5 +1,6 @@
 from pychecker2 import TestSupport
 from pychecker2 import ParseChecks
+from pychecker2 import VariableChecks
 from pychecker2.File import File
 
 class UnknownTestCase(TestSupport.WarningTester):
@@ -10,3 +11,14 @@ class UnknownTestCase(TestSupport.WarningTester):
         self.checklist.check_file(f)
         self.warning_file(f, 0, ParseChecks.ParseCheck.syntaxErrors,
                           "No such file or directory")
+
+        self.warning('if __name__=="__main__":\n'
+                     '  _x = 1\n',
+                     2, VariableChecks.UnusedCheck.unused, '_x')
+        self.argv = ['--no-main']
+        self.silent('NoMoreGood = 1\n'
+                    'assert NoMoreGood\n')
+        self.silent('if __name__=="__main__":\n'
+                     '  _x = 1\n')
+        self.silent('if "__main__"==__name__:\n'
+                     '  _x = 1\n')
