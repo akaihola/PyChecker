6368248a46f15a748027979525ef0a099fb5abba
ericnewton <ericnewton>
2002-04-04 20:52:25 +0000

    test empty exceptions

--- pychecker2/utest/ops.py
+++ pychecker2/utest/ops.py
@@ -11,3 +11,21 @@ class OpTests(TestSupport.WarningTester):
     def testOperatorPlus(self):
         self.warning('def f(x):\n'
                      '   return +x', 2, OpChecks.OpCheck.operatorPlus)
+
+    def testExcept(self):
+        self.warning('try:\n'
+                     '   pass\n'
+                     'except:\n'
+                     '   pass\n', 4, OpChecks.ExceptCheck.emptyExcept)
+        self.warning('try:\n'
+                     '   pass\n'
+                     'except AssertionError:\n'\
+                     '   pass\n'
+                     'except:\n'
+                     '   pass\n', 6, OpChecks.ExceptCheck.emptyExcept)
+        self.warning('try:\n'
+                     '   pass\n'
+                     'except:\n'
+                     '   pass\n'
+                     'except AssertionError:\n'\
+                     '   pass\n', 4, OpChecks.ExceptCheck.emptyExcept)
