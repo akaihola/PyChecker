28c86030645a8ce4e62210ec3ac87499671c6e4e
ericnewton <ericnewton>
2002-04-06 23:05:08 +0000

    had attribute-not-initialized warning... too much noise

--- pychecker2/scripts/lib.sh
+++ pychecker2/scripts/lib.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-scripts/pychecker2.sh --incremental --no-importError --no-unused --no-shadowIdentifier --no-shadowImport --no-emptyExcept /usr/local/lib/python2.2/*.py
+scripts/pychecker2.sh --incremental --no-importError --no-unused --no-shadowIdentifier --no-shadowImport --no-emptyExcept --no-attributeInitialized /usr/local/lib/python2.2/*.py
