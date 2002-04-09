02be328b3b5dd54c686072aa7008862f044fdb38
ericnewton <ericnewton>
2002-04-09 17:46:13 +0000

    simplify lineno code add utility to get all scopes, except class scopes

--- pychecker2/File.py
+++ pychecker2/File.py
@@ -14,28 +14,26 @@ class File:
         return cmp(self.name, other.name)
 
     def warning(self, line, warn, *args):
-        lineno = line
-	try:
-	    lineno = line.lineno
-        except AttributeError:
-	    pass
-        if not lineno:
-            try:
-                for p in parents(line):
-                    if p.lineno:
-                        lineno = p.lineno
-                        break
-            except AttributeError:
-                pass
+        lineno = getattr(line, 'lineno', line)
+        if not lineno and hasattr(line, 'parent'):
+            for p in parents(line):
+                if p.lineno:
+                    lineno = p.lineno
+                    break
         self.warnings.append( (lineno, warn, args) )
 
     def scope_filter(self, type):
-        return [(n, s)
-                for n, s in self.scopes.iteritems() if isinstance(n, type)
-                ]
+        return [x for x in self.scopes.iteritems() if isinstance(x[0], type)]
 
     def function_scopes(self):
         return self.scope_filter(ast.Function)
 
     def class_scopes(self):
         return self.scope_filter(ast.Class)
+
+    def not_class_scopes(self):
+        result = []
+        for n, s in self.scopes.iteritems():
+            if not isinstance(n, ast.Class):
+                result.append( (n, s) )
+        return result
