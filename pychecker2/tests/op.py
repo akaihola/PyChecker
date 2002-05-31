589a03d7cdc086ac35652e9723936534a9722684
montanaro <montanaro>
2002-05-31 21:28:33 +0000

    add comparison check for add test for "== None"

--- pychecker2/tests/op.py
+++ pychecker2/tests/op.py
@@ -6,3 +6,14 @@ def g(a):
 
 def g(a):
     return --a
+
+def foo():
+    if 1 == None:
+        print "whoa!"
+    else:
+        print "whew!"
+
+    if None == 1:
+        print "whoa!"
+    else:
+        print "whew!"
