c6d76d9ce4a3d9acc11be0b8d821abaf2ac2552d
nnorwitz <nnorwitz>
2005-09-04 21:08:11 +0000

    why not make a new release so there are no more bugs about setup failing.  version numbers are cheap

--- pychecker/Config.py
+++ pychecker/Config.py
@@ -15,7 +15,7 @@ import time
 
 _RC_FILE = ".pycheckrc"
 CHECKER_VAR = '__pychecker__'
-_VERSION = '0.8.15'
+_VERSION = '0.8.16'
 
 _DEFAULT_BLACK_LIST = [ "Tkinter", "wxPython", "gtk", "GTK", "GDK", ]
 _DEFAULT_VARIABLE_IGNORE_LIST = [ '__version__', '__warningregistry__', 
