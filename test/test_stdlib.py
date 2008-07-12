c2bee1a14f1e6fc4d4cb6c61d6a6f913d2a46567
thomasvs <thomasvs>
2008-07-12 10:01:28 +0000

    * test/test_stdlib.py: 	  Rename and document test.

--- test/test_stdlib.py
+++ test/test_stdlib.py
@@ -1,10 +1,20 @@
 # -*- Mode: Python -*-
 # vi:si:et:sw=4:sts=4:ts=4
 
+'''
+Tests related to the -q/--stdlib option.
+'''
+
 import unittest
 import common
 
-class MyTestCase(common.TestCase):
+class ZopeTestCase(common.TestCase):
+    '''
+    test that -q properly ignores errors for modules in the arch-specific
+    directory.
+
+    See http://sourceforge.net/tracker/index.php?func=detail&aid=1564614&group_id=24686&atid=382217
+    '''
     def test_zope_interface(self):
         if not common.can_import('zope.interface'):
             self.skip = True # FIXME: interpret this
