3e575f784a0c8fa0a9232af2967206286ec26494
thomasvs <thomasvs>
2010-12-29 13:14:53 +0000

    * TODO: 	  Add a TODO item. 	* test/test_pychecker_function.py: 	  Add a unit test for function.Function 	  Add a surprising result for co_varnames for generators.

---
+++ test/test_pychecker_function.py
@@ -0,0 +1,38 @@
+# -*- Mode: Python; test-case-name: test.test_pychecker_function -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+'''
+Tests related to pychecker.function
+'''
+
+import unittest
+import common
+
+from pychecker import function
+
+class GeneratorTestCase(common.TestCase):
+    '''
+    Test that a generator function works as expected.
+    '''
+    def testGenerator(self):
+        def returner():
+            return (str(x) for x in range(10))
+
+        # get the generator code object
+        genCode = returner.func_code.co_consts[1]
+
+        # FIXME: this is what co_varnames looks like, but I don't understand why
+        # possible clue in Python, Lib/compiler/ast.py, class GenExpr
+        self.assertEquals(genCode.co_varnames, ('.0', 'x'))
+
+        # wrap it into a Funtion so we can look at it
+        f = function.Function(
+            function.FakeFunction(genCode.co_name, genCode))
+
+        self.failIf(f.isMethod)
+        self.assertEquals(f.minArgs, 1)
+        self.assertEquals(f.maxArgs, 1)
+        self.assertEquals(f.arguments(), ('.0', ))
+
+if __name__ == '__main__':
+    unittest.main()
