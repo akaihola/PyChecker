58809a7dfd4f8ba81bd317fee65071caaf21d50d
thomasvs <thomasvs>
2011-01-08 11:55:57 +0000

    * setup.py: 	  Back to HEAD.

--- setup.py
+++ setup.py
@@ -252,7 +252,7 @@ however, spurious warnings should be fairly infrequent.
 """
 
 kw =  { 'name'             : "pychecker",
-        'version'          : "0.8.19",
+        'version'          : "0.8.19.1",
         'license'          : "BSD-like",
         'description'      : "Python source code checking tool",
         'author'           : "Neal Norwitz",
